ebec60288e50884992caa8bb2fcd76691e8a197c
SonnyHardy <htambeck@gmail.com>
2024-07-19 20:39:04 +0200

    Projet initial

---
+++ src/main/java/com/sonny/ctnotification/CtNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.sonny.ctnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sonny/ctnotification/CtNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonny.ctnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
